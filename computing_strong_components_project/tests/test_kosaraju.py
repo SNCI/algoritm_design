f976fd23a85cc4d47523168476486d697f3fc353
Drew Verlee <Drew.verlee@gmail.com>
2013-07-22 16:54:02 -0400

    passing steckos tests

--- computing_strong_components_project/tests/test_kosaraju.py
+++ computing_strong_components_project/tests/test_kosaraju.py
@@ -8,15 +8,39 @@ Description : test cases for Coursera Algorithms 1 class assignment 4
 """
 
 import unittest
-from computing_strong_components_project.main.kosarajus import kosaraju
+from collections import defaultdict, Counter
+from computing_strong_components_project import kosarajus
 class TestKosaraju(unittest.TestCase):
     """test kosarajus Algorithm against files contained in data"""
 
+    def setUp(self):
+        kosarajus.s = None # leaders in 2nd pass
+        kosarajus.explored = set()
+        kosarajus.leaders = Counter()
+        kosarajus.vertex_to_finishing_time = defaultdict(int)
+        kosarajus.t = 0
+        kosarajus.second_loop = False
+
 
     def test_class_example(self):
-        # how to represent the graph?
         expected = "3,3,3,0,0"
-        result = kosaraju('../data/test_data_from_class_kosaraju.txt')
+        result = kosarajus.kosaraju('../data/test_data_from_class_kosaraju.txt')
+        self.assertSequenceEqual(expected, result)
+
+    # def test_unconnected(self):
+    #     """ not actual sure about this one"""
+    #     expected = "1,1,0,0,0"
+    #     result = kosarajus.kosaraju('../data/test_data_kosaraju_unconnected.txt')
+    #     self.assertSequenceEqual(expected, result)
+
+    def test_data_kosaraju_Stecko_case_1(self):
+        expected = '3,3,1,1,0'
+        result = kosarajus.kosaraju('../data/test_data_kosaraju_Stecko_case_1.txt')
+        self.assertSequenceEqual(expected, result)
+
+    def test_data_kosaraju_Stecko_case_2(self):
+        expected = '7,1,0,0,0'
+        result = kosarajus.kosaraju('../data/test_data_kosaraju_Stecko_case_2.txt')
         self.assertSequenceEqual(expected, result)
 
 
