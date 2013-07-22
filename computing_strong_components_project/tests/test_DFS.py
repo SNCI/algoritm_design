21e19d77da0bbd4108a85a8575f18c4c0e8f9cab
Drew Verlee <Drew.verlee@gmail.com>
2013-07-22 03:37:18 -0400

    computing components first commit

---
+++ computing_strong_components_project/tests/test_DFS.py
@@ -0,0 +1,18 @@
+"""
+       File : test_DFS.py
+     Author : Drew Verlee
+       Date : 21.07.13.
+      Email : drew.verlee@gmail.com
+     GitHub : https://github.com/Midnightcoffee/
+Description : tests for depth first search
+"""
+
+
+import unittest
+
+class TestDFS(unittest.TestCase):
+    """test cases for depth first search"""
+
+
+    def test_small_DFS(self):
+        pass
---
+++ computing_strong_components_project/tests/test_kosaraju.py
@@ -0,0 +1,21 @@
+"""
+       File : test_kosaraju.py
+     Author : Drew Verlee
+       Date : 22.07.13.
+      Email : drew.verlee@gmail.com
+     GitHub : https://github.com/Midnightcoffee/
+Description : test cases for Coursera Algorithms 1 class assignment 4
+"""
+
+import unittest
+from computing_strong_components_project.main.kosarajus import kosaraju
+class TestKosaraju(unittest.TestCase):
+    """test kosarajus Algorithm against files contained in data"""
+
+
+    def test_class_example(self):
+        # how to represent the graph?
+        expected = "3,3,3,0,0"
+        result = kosaraju('../data/test_data_from_class_kosaraju.txt')
+        self.assertSequenceEqual(expected, result)
+
